3335eb6ee4a93ea832990b5e345a80e4bc6362f5
Amanola Panara-ag <panaraagamanola@gmail.com>
2023-09-22 09:32:53 +0800

    working

---
+++ lib/data/person.dart
@@ -0,0 +1,25 @@
+class Person {
+  final String id;
+  final String firstName;
+  final String middleName;
+  final String lastName;
+  final String motherId;
+  final String fatherId;
+  final String location;
+  final String birthday;
+  final String gender;
+  final String spouse;
+
+  Person({
+    required this.id,
+    required this.firstName,
+    required this.middleName,
+    required this.lastName,
+    required this.motherId,
+    required this.fatherId,
+    required this.location,
+    required this.birthday,
+    required this.gender,
+    required this.spouse,
+  });
+}
---
+++ lib/utils.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:marandacan/data/person.dart';
+
+bool isInteger(String input) {
+  final RegExp integerRegExp = RegExp(r'^[-+]?[0-9]+$');
+  return integerRegExp.hasMatch(input);
+}
+
+String validateString(String value) {
+  return value == '_DEF' ? '' : value;
+}
+
+String findFullNameById(String id, List<Person> personList) {
+  final matchingPerson = personList.firstWhere((person) => person.id == id,
+      orElse: () => Person(
+            id: '',
+            firstName: '',
+            middleName: '',
+            lastName: '',
+            motherId: '',
+            fatherId: '',
+            location: '',
+            birthday: '',
+            gender: '',
+            spouse: '',
+          ));
+
+  String fullName = '${validateString(matchingPerson.firstName)} '
+      '${validateString(matchingPerson.middleName)} '
+      '${validateString(matchingPerson.lastName)}';
+
+  if (fullName.trim() == '') {
+    if (validateString(id) == '') return 'UNKNOWN';
+    return validateString(id);
+  } else {
+    return fullName;
+  }
+}
+
+List<Person> filterListById(String id, List<Person> peopleList) {
+  return peopleList
+      .where((person) => person.fatherId == id || person.motherId == id)
+      .toSet() // Convert to a set to remove duplicates
+      .toList(); // Convert back to a list
+}
+
+//Widgets
+void showPersonInfo(BuildContext context, Person person, List<Person> people) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text(
+          'Personal Information',
+          style: TextStyle(
+            color: Colors.green, // Set the text color to green
+          ),
+        ),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            buildInfoRow(context, 'Full Name',
+                '${validateString(person.gender)} ${validateString(person.firstName)} ${validateString(person.middleName)} ${validateString(person.lastName)}'),
+            buildInfoRow(context, 'Birthday', validateString(person.birthday)),
+            buildInfoRow(context, 'Location', validateString(person.location)),
+            buildInfoRow(context, 'Father\'s Name', findFullNameById(person.fatherId, people)),
+            buildInfoRow(context, 'Mother\'s Name', findFullNameById(person.motherId, people)),
+            buildInfoRow(context, 'Spouse\'s Name', findFullNameById(person.spouse, people)),
+          ],
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Close'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+Widget buildInfoRow(BuildContext context, String label, String value) {
+  return RichText(
+    text: TextSpan(
+      children: <TextSpan>[
+        TextSpan(
+          text: '\n$label\n',
+          style: const TextStyle(
+            fontWeight: FontWeight.normal,
+            fontSize: 12,
+            color: Colors.grey, // Set label color here
+          ),
+        ),
+        TextSpan(
+          text: value,
+          style: const TextStyle(
+            fontSize: 14,
+            color: Colors.black, // Set value color here
+          ),
+        ),
+      ],
+    ),
+  );
+}
